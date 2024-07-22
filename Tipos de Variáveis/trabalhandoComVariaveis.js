1cfcfaf278c2e8939e25e35fa5ac2be33d992ff8
Lucas <lucas.emanuel.carvalho@outlook.com>
2024-07-22 15:10:38 -0300

    Primeiro tópico

---
+++ Tipos de Variáveis/trabalhandoComVariaveis.js
@@ -0,0 +1,8 @@
+// Console.log("Trabalhando com variáveis");
+console.log("Trablhando com variáveis");
+// O JS é case sensitive e colocar ; no final é opcional para o JS, mas é uma prática interessante.
+idade = 10;
+Idade = 20;
+
+console.log(idade);
+console.log(Idade);
