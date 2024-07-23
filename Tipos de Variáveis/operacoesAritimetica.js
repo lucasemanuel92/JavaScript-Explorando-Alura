ee9487e0c952201b048242bc1f62853a03f75e2a
Lucas <lucas.emanuel.carvalho@outlook.com>
2024-07-23 11:21:50 -0300

    operações

---
+++ Tipos de Variáveis/operacoesAritimetica.js
@@ -0,0 +1,10 @@
+console.log("Operações Aritiméticas");
+
+// É possivel realizar operações diretas, o computador faz operações em ordem
+console.log(10 + 8 * 2);
+console.log((10 + 8) * 2);
+
+// Na mistura de texto e numero o sinal de "+" é usado para concatenar os tipos
+console.log("ano " + 2020);
+console.log("2" + "2");
+console.log("2" + 2);
--- Tipos de Variáveis/trabalhandoComVariaveis.js
+++ Tipos de Variáveis/trabalhandoComVariaveis.js
@@ -1,8 +1,14 @@
 // Console.log("Trabalhando com variáveis");
 console.log("Trablhando com variáveis");
 // O JS é case sensitive e colocar ; no final é opcional para o JS, mas é uma prática interessante.
+
 idade = 10;
 Idade = 20;
 
 console.log(idade);
 console.log(Idade);
+// É uma má prática usar letras maiúsculas e minúsculas dessa forma.
+
+const nome = "Lucas";
+let anoNascimento = 1992;
+// É padrão usar const ou let para declarar variáveis, apesar de não ser necessário 
