f9cf8658ee331d879310d318de297d86def5ff66
Lucas <lucas.emanuel.carvalho@outlook.com>
2024-07-23 11:29:54 -0300

    conversão

---
+++ Tipos de Variáveis/conversaoDeTipos.js
@@ -0,0 +1,19 @@
+console.log("Conversão de Tipos");
+
+// O operador "+" para textos concatena, ou seja, junta os textos
+console.log("2" + "2")
+
+// "parseInt" é o comando para conversão de texto para int, posso usar outros tipos como parse
+console.log(parseInt("2") + parseInt("2"));
+
+// Para outras operações ele converte de forma normal
+console.log("10" / "2");
+console.log("10" * "2");
+console.log("3" - "2");
+
+// Caso os tipos não sejam iguais para fazer uma operação ele retorna um NaN ou "Not a Number"
+console.log("Lucas" * "2") 
+
+// Se usa o "." no lugar das "," comum nas operações brasileiras
+console.log(6.5);
+console.log(6,5); // Gera um espaço
