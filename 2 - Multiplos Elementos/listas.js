1c873f362063c32c060df895ccf3d7e4e58f924a
Lucas <lucas.emanuel.carvalho@outlook.com>
2024-07-23 12:32:12 -0300

    lists

--- 2 - Multiplos Elementos/listas.js
+++ 2 - Multiplos Elementos/listas.js
@@ -1,11 +1,11 @@
 console.log(`Trabalhando com Listas`);
 
 // Essa não é uma boa forma
-const salvador = `Salvador`;
-const rio = `Rio de Janeiro`;
-const sampa = `São Paulo`;
+// const salvador = `Salvador`;
+// const rio = `Rio de Janeiro`;
+// const sampa = `São Paulo`;
 
-console.log(salvador, rio, sampa);
+// console.log(salvador, rio, sampa);
 
 // Forma com array
 const listaDeDestinos = new Array(
@@ -19,3 +19,9 @@ console.log(listaDeDestinos);
 
 listaDeDestinos.push("Porto Alegre"); // ".push()" adiciona um novo elemento a lista
 console.log(listaDeDestinos);
+
+// ".splice(posição do elemento, quantos a partir dele quero deletar)"
+listaDeDestinos.splice(2, 1); 
+console.log(listaDeDestinos);
+// Mostrar só a posição que eu quero mostrar
+console.log(listaDeDestinos[2])
